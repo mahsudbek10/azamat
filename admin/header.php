6cf0277830868ebfc8901f3f2e7fc7e2a858689f
m.kurvanbayev <m.kurvanbayev@DESKTOP-LO6NPI7>
2021-06-13 22:00:46 +0600

    edit all php filesss vbvnbvn cvbn cbnvb

--- admin/header.php
+++ admin/header.php
@@ -1,6 +1,6 @@
 <?php
 if(session_status() != PHP_SESSION_ACTIVE) session_start ();
-if(!isset($_SESSION['auth']))    header("Location: ../index");
+//if(!isset($_SESSION['auth']))    header("Location: ../index");
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
